3f51cf05b7623ff5cee2be5b1b18ba1c39caea09
lsj921008 <90734789+lsj921008@users.noreply.github.com>
2021-12-01 08:47:52 +0800

    print my name with line break

--- myname.c
+++ myname.c
@@ -1,5 +1,6 @@
-# National Hsinchu Senior Industrial Vocational School
-## Programming, Introduction of Computer
-### 3rd grade
-- langugage: C
-- topic: stdio
+#include <stdio.h>
+
+main() {
+
+    printf("羅嵩傑\n");
+}
