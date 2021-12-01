22b54ad9e51f067c58f1582484b9c0e851ecb813
lsj921008 <90734789+lsj921008@users.noreply.github.com>
2021-12-01 08:45:05 +0800

    Update myname.c

--- myname.c
+++ myname.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-
-main() {
-
-    printf("羅嵩傑\n");
-}
+# National Hsinchu Senior Industrial Vocational School
+## Programming, Introduction of Computer
+### 3rd grade
+- langugage: C
+- topic: stdio
